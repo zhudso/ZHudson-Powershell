c97634ee6b716413810c0d8e997063c99cd94cae
Zach Hudson <zacharylhud@gmail.com>
2021-08-23 18:41:24 -0500

    v.02

--- Office 365/Reset-O365Password.ps1
+++ Office 365/Reset-O365Password.ps1
@@ -60,20 +60,24 @@ Write-Host "Connecting to Azure-AD"
         Get-MsolUser | Where-Object {($_.Islicensed -eq $true) -and ($_.PasswordNeverExpires -eq $true)} | Select-Object Displayname,PasswordNeverExpires
     }
 
-#Find users that are licensed and passwords has NOT been changed within the last 7 days.
-Write-Host "Checking for any users that have not changed their password in the last 7 days.."
-    $pwdResetUsers = Get-MsolUser | Where-Object {($_.Islicensed -eq $true) -and ($_.LastPasswordChangeTimestamp -lt (Get-Date).AddDays(7))}
-#Go through each user who's password hasn't been updated in the last 7 days to force password change at next logon.
+#Find users that are licensed and passwords has NOT been changed within the last 11 days.
+Write-Host "Checking for any users that have not changed their password in the last 11 days.."
+    $pwdResetUsers = Get-MsolUser | Where-Object {($_.Islicensed -eq $true) -and ($_.LastPasswordChangeTimestamp -lt (Get-Date).AddDays(-11))}
+#Go through each user who's password hasn't been updated in the last 11 days to force password change at next logon.
+    $i = 0
     foreach ($user in $pwdResetUsers) {
-        Write-Progress -Activity 'Processing Pwd Reset Flags & Removing Access Tokens..'
+        Write-Progress -Activity 'Processing Pwd Reset Flags & Removing Access Tokens..' -Status "Scanned: $i of $($pwdResetUsers.Count)" -PercentComplete (($i / $pwdResetUsers) * 100)
         #Flag account for password reset at next logon.
         Get-MsolUser -UserPrincipalName $user.UserPrincipalName | Set-MsolUserPassword -ForceChangePasswordOnly $true -ForceChangePassword $true
         #Revoke Access Token
         Get-AzureADUser -SearchString $user.UserPrincipalName | Revoke-AzureADUserAllRefreshToken
     }
-Write-Host -ForegroundColor Yellow "Checking for any Refresh Tokens that weren't deleted.."
-Get-AzureADUser -SearchString $user | Select-Object *refresh*
+Write-Host -foregroundcolor Yellow "Users that were effected"
+$pwdResetUsers | Select-Object UserPrincipalName, DisplayName, LastPasswordChangeTimestamp, PasswordNeverExpires
+
+<# Write-Host -ForegroundColor Yellow "Checking for any Refresh Tokens that weren't deleted.."
+Get-AzureADUser -SearchString $user | Select-Object *refresh* #>
     
-Write-Host "Script completed, instruct users to close their browser and navigate to https://www.office.com to update their passwords.
-'Refresh Tokens' are kept for 5 hours by default. If any were provided above, then run command: Get-AzureADUser -SearchString 'USERS EMAIL ADDRESS' | Revoke-AzureADUserAllRefreshToken"
+Write-Host -ForegroundColor Cyan "Script completed, instruct users to close their browser and navigate to https://www.office.com to update their passwords."
+Write-host "Refresh Tokens are kept for 5 hours by default. If any were output above, then run command: Get-AzureADUser -SearchString 'USERS EMAIL ADDRESS' | Revoke-AzureADUserAllRefreshToken (Do this for each user listed)"
 }
